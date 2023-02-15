262818eb774d63e7248a494053f43f5b8640567d
kiootic <kiootic@oursky.com>
2023-02-15 15:33:17 +0800

    Add experimental low-level authenticate functions

--- Sources/AuthgearExperimental.swift
+++ Sources/AuthgearExperimental.swift
@@ -3,8 +3,64 @@ import Foundation
 public struct AuthgearExperimental {
     let authgear: Authgear
 
+    public struct AuthorizationRequest {
+        public let url: URL
+        public let redirectURI: String
+        let verifier: CodeVerifier
+    }
+
     public func generateURL(redirectURI: String, handler: URLCompletionHandler?) {
         self.authgear.generateURL(redirectURI: redirectURI, handler: handler)
+    }
+
+    public func createAuthenticateRequest(
+        redirectURI: String,
+        state: String? = nil,
+        prompt: [PromptOption]? = nil,
+        loginHint: String? = nil,
+        uiLocales: [String]? = nil,
+        colorScheme: ColorScheme? = nil,
+        wechatRedirectURI: String? = nil,
+        page: AuthenticationPage? = nil,
+        customUIQuery: String? = nil
+    ) -> Result<AuthorizationRequest, Error> {
+        let options = AuthenticateOptions(
+            redirectURI: redirectURI,
+            isSSOEnabled: self.authgear.isSSOEnabled,
+            state: state,
+            prompt: prompt,
+            loginHint: loginHint,
+            uiLocales: uiLocales,
+            colorScheme: colorScheme,
+            wechatRedirectURI: wechatRedirectURI,
+            page: page,
+            customUIQuery: customUIQuery
+        )
+        return self.authgear.createAuthenticateRequest(options).map { request in
+            AuthorizationRequest.fromInternal(request)
+        }
+    }
+
+    public func finishAuthentication(
+        finishURL: URL,
+        request: AuthgearExperimental.AuthorizationRequest,
+        handler: @escaping UserInfoCompletionHandler
+    ) {
+        self.authgear.finishAuthentication(
+            url: finishURL,
+            request: request.toInternal(),
+            handler: handler
+        )
+    }
+}
+
+extension AuthgearExperimental.AuthorizationRequest {
+    static func fromInternal(_ request: AuthorizationRequest) -> Self {
+        Self(url: request.url, redirectURI: request.redirectURI, verifier: request.verifier)
+    }
+
+    func toInternal() -> AuthorizationRequest {
+        AuthorizationRequest(url: self.url, redirectURI: self.redirectURI, verifier: self.verifier)
     }
 }
 
