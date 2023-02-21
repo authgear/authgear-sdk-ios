5eaf9fb6f4322528cbd1e293596140b676a2c670
kiootic <kiootic@oursky.com>
2023-02-21 11:31:08 +0800

    Add Latte helper functions

---
+++ Sources/Latte+Flows.swift
@@ -0,0 +1,183 @@
+import Foundation
+import UIKit
+
+@available(iOS 13.0, *)
+public extension Latte {
+    struct Handle<T> {
+        let viewController: UIViewController?
+        public let result: Result<T, Error>
+
+        public func popViewController(animated: Bool) {
+            self.viewController?.navigationController?.popViewController(animated: animated)
+        }
+    }
+
+    typealias ResultHandler<T> = (Handle<T>) -> Void
+
+    static func authenticate(
+        context: UIViewController,
+        authgear: Authgear,
+        redirectURI: String,
+        state: String? = nil,
+        prompt: [PromptOption]? = nil,
+        loginHint: String? = nil,
+        uiLocales: [String]? = nil,
+        colorScheme: ColorScheme? = nil,
+        wechatRedirectURI: String? = nil,
+        page: AuthenticationPage? = nil,
+        customUIQuery: String? = nil,
+        handler: @escaping ResultHandler<UserInfo>
+    ) {
+        Task { await run() }
+
+        @Sendable @MainActor
+        func run() async {
+            var viewController: LatteViewController?
+            do {
+                let request = try authgear.experimental.createAuthenticateRequest(
+                    redirectURI: redirectURI,
+                    state: state,
+                    prompt: prompt,
+                    loginHint: loginHint,
+                    uiLocales: uiLocales,
+                    colorScheme: colorScheme,
+                    wechatRedirectURI: wechatRedirectURI,
+                    page: page,
+                    customUIQuery: customUIQuery
+                ).get()
+
+                let webViewRequest = LatteWebViewRequest(request: request)
+                let latteVC = LatteViewController(context: context, request: webViewRequest)
+                viewController = latteVC
+
+                let result: LatteWebViewResult = try await withCheckedThrowingContinuation { next in
+                    latteVC.handler = { next.resume(with: $0) }
+                    context.navigationController?.pushViewController(latteVC, animated: true)
+                }
+
+                let userInfo: UserInfo = try await withCheckedThrowingContinuation { resume in
+                    authgear.experimental.finishAuthentication(finishURL: result.finishURL, request: request) { resume.resume(with: $0)
+                    }
+                }
+                handler(Handle(viewController: viewController, result: .success(userInfo)))
+            } catch {
+                handler(Handle(viewController: viewController, result: .failure(error)))
+            }
+        }
+    }
+
+    static func verifyEmail(
+        context: UIViewController,
+        authgear: Authgear,
+        email: String,
+        entryURL: String,
+        redirectURI: String,
+        handler: @escaping ResultHandler<UserInfo>
+    ) {
+        Task { await run() }
+
+        @Sendable @MainActor
+        func run() async {
+            var viewController: LatteViewController?
+            do {
+                let urlQueryAllowed = CharacterSet.urlQueryAllowed.subtracting(["+"])
+                let query = [
+                    "email=\(email.addingPercentEncoding(withAllowedCharacters: urlQueryAllowed)!)",
+                    "redirect_uri=\(redirectURI.addingPercentEncoding(withAllowedCharacters: urlQueryAllowed)!)"
+                ].joined(separator: "&")
+                let verifyEmailURL = "\(entryURL)?\(query)"
+
+                let url: URL = try await withCheckedThrowingContinuation { resume in
+                    authgear.experimental.generateURL(redirectURI: verifyEmailURL) {
+                        resume.resume(with: $0)
+                    }
+                }
+
+                let webViewRequest = LatteWebViewRequest(url: url, redirectURI: redirectURI)
+                let latteVC = LatteViewController(context: context, request: webViewRequest)
+                viewController = latteVC
+
+                let result: LatteWebViewResult = try await withCheckedThrowingContinuation { next in
+                    latteVC.handler = { next.resume(with: $0) }
+                    context.navigationController?.pushViewController(latteVC, animated: true)
+                }
+
+                let components = URLComponents(url: result.finishURL, resolvingAgainstBaseURL: false)!
+                if let urlError = components.queryItems?.first(where: { $0.name == "error" })?.value {
+                    let error: AuthgearError
+                    if urlError == "cancel" {
+                        error = .cancel
+                    } else {
+                        error = .oauthError(OAuthError(error: urlError, errorDescription: nil, errorUri: nil))
+                    }
+                    handler(Handle(viewController: viewController, result: .failure(error)))
+                    return
+                }
+
+                let userInfo: UserInfo = try await withCheckedThrowingContinuation { resume in
+                    authgear.experimental.authgear.fetchUserInfo() { resume.resume(with: $0) }
+                }
+                handler(Handle(viewController: viewController, result: .success(userInfo)))
+            } catch {
+                handler(Handle(viewController: viewController, result: .failure(error)))
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+private class LatteViewController: UIViewController, LatteWebViewDelegate {
+    weak var context: UIViewController?
+    let webView: LatteWKWebView
+    var handler: ((Result<LatteWebViewResult, Error>) -> Void)?
+
+    init(
+        context: UIViewController,
+        request: LatteWebViewRequest
+    ) {
+        self.context = context
+        self.webView = LatteWKWebView(request)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addSubview(self.webView)
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.webView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.webView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        self.webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+
+        self.webView.delegate = self
+
+        self.webView.load()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func latteWebView(completed _: LatteWebView, result: Result<LatteWebViewResult, Error>) {
+        self.handler?(result)
+    }
+
+    func latteWebView(onEvent _: LatteWebView, event: LatteWebViewEvent) {
+        switch event {
+        case .openEmailClient:
+            let items = [
+                Latte.EmailClient.mail,
+                Latte.EmailClient.gmail
+            ]
+            let alert = Latte.makeChooseEmailClientAlertController(
+                title: "Open mail app",
+                message: "Which app would you like to open?",
+                cancelLabel: "Cancel",
+                items: items
+            )
+            self.context?.present(alert, animated: true)
+        }
+    }
+}
