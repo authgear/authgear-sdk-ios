5dc706f895a7b93b56cf2ceab9482df6b2a2ec1e
Tung Wu <tung@oursky.com>
2023-02-22 16:57:39 +0800

    Update latte universal link handler interface

--- Sources/Latte+UniversalLink.swift
+++ Sources/Latte+UniversalLink.swift
@@ -2,59 +2,71 @@ import Foundation
 import UIKit
 
 @available(iOS 13.0, *)
-public extension Latte {
-    static func handleUniversalLink(
+public protocol LatteLinkHandler {
+    func handle(
         context: UINavigationController,
         authgear: Authgear,
+        handler: @escaping Latte.ResultHandler<Void>
+    )
+}
+
+@available(iOS 13.0, *)
+public extension Latte {
+    struct ResetLinkHandler: LatteLinkHandler {
+        let customUIEndpoint: String
+        let query: [URLQueryItem]?
+
+        public func handle(
+            context: UINavigationController,
+            authgear: Authgear,
+            handler: @escaping Latte.ResultHandler<Void>) {
+            Task { await run() }
+            @Sendable @MainActor
+            func run() async {
+                var viewController: LatteViewController?
+                do {
+                    var entryURLComponents = URLComponents(string: customUIEndpoint + "/recovery/reset")!
+                    entryURLComponents.queryItems = query
+                    let entryURL = entryURLComponents.url!
+                    let redirectURI = customUIEndpoint + "/recovery/reset/complete"
+                    let webViewRequest = LatteWebViewRequest(url: entryURL, redirectURI: redirectURI)
+                    let latteVC = LatteViewController(context: context, request: webViewRequest)
+                    viewController = latteVC
+                    let _ = try await withCheckedThrowingContinuation { next in
+                        latteVC.handler = { next.resume(with: $0) }
+                        context.pushViewController(latteVC, animated: true)
+                    }
+                    handler(Handle(isPresented: false, viewController: viewController, result: .success(())))
+                } catch {
+                    handler(Handle(isPresented: false, viewController: viewController, result: .failure(error)))
+                }
+            }
+        }
+    }
+    
+    
+    static func getUniversalLinkHandler(
+        context: UINavigationController,
         customUIEndpoint: String,
+        linkURLHost: String,
         userActivity: NSUserActivity
-    ) -> Bool {
+    ) -> LatteLinkHandler? {
         guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
               let incomingURL = userActivity.webpageURL,
               let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
-            return false
+            return nil
         }
         guard let path = components.path,
-              let params = components.queryItems else {
-            return false
+              let host = components.host, host == linkURLHost else {
+            return nil
         }
         switch path {
         case _ where path.hasSuffix("/reset_link"):
-            Latte.handleResetLink(
-                context: context,
-                authgear: authgear,
+            return ResetLinkHandler(
                 customUIEndpoint: customUIEndpoint,
-                query: params
-            )
-            return true
+                query: components.queryItems)
         default:
-            return false
-        }
-    }
-
-    private static func handleResetLink(
-        context: UINavigationController,
-        authgear: Authgear,
-        customUIEndpoint: String,
-        query: [URLQueryItem]?
-    ) {
-        Task { await run() }
-        @Sendable @MainActor
-        func run() async {
-            do {
-                var entryURLComponents = URLComponents(string: customUIEndpoint + "/recovery/reset")!
-                entryURLComponents.queryItems = query
-                let entryURL = entryURLComponents.url!
-                let redirectURI = customUIEndpoint + "/recovery/reset/complete"
-                let webViewRequest = LatteWebViewRequest(url: entryURL, redirectURI: redirectURI)
-                let latteVC = LatteViewController(context: context, request: webViewRequest)
-                let _ = try await withCheckedThrowingContinuation { next in
-                    latteVC.handler = { next.resume(with: $0) }
-                    context.pushViewController(latteVC, animated: true)
-                }
-            } catch {
-                print(error)
-            }
+            return nil
         }
     }
 }
