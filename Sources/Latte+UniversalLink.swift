0840f2f7ff78cc29c0aa743e1fff44ea616ba775
Louis Chan <louischan@oursky.com>
2023-02-23 15:00:27 +0900

    Support rewriteUniversalLinkOrigin

--- Sources/Latte+UniversalLink.swift
+++ Sources/Latte+UniversalLink.swift
@@ -53,29 +53,62 @@ public extension Latte {
     }
 
     static func getUniversalLinkHandler(
-        linkURLHost: String,
-        userActivity: NSUserActivity
+        userActivity: NSUserActivity,
+        universalLinkOrigin: URL,
+        rewriteUniversalLinkOrigin: URL? = nil
     ) -> LatteLinkHandler? {
         guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
               let incomingURL = userActivity.webpageURL,
               let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
             return nil
         }
-        guard let path = components.path,
-              let host = components.host, host == linkURLHost else {
+
+        guard incomingURL.origin == universalLinkOrigin.origin else {
             return nil
         }
+
+        guard let path = components.path else {
+            return nil
+        }
+
         switch path {
         case _ where path.hasSuffix("/reset_link"):
             return ResetLinkHandler(
                 query: components.queryItems
             )
         case _ where path.hasSuffix("/login_link"):
+            let url: URL
+            if let rewriteUniversalLinkOrigin = rewriteUniversalLinkOrigin {
+                url = incomingURL.rewriteOrigin(origin: rewriteUniversalLinkOrigin)
+            } else {
+                url = incomingURL
+            }
             return LoginLinkHandler(
-                url: incomingURL
+                url: url
             )
         default:
             return nil
         }
+    }
+}
+
+private extension URL {
+    var origin: URL {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        components.fragment = nil
+        components.password = nil
+        components.path = ""
+        components.query = nil
+        components.user = nil
+        return components.url!
+    }
+
+    func rewriteOrigin(origin: URL) -> URL {
+        var selfComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        var thatComponents = URLComponents(url: origin, resolvingAgainstBaseURL: true)!
+        selfComponents.scheme = thatComponents.scheme
+        selfComponents.host = thatComponents.host
+        selfComponents.port = thatComponents.port
+        return selfComponents.url!
     }
 }
