8f985fb0df862f9410aa5dabb8b0061c61413754
Tung Wu <tung@oursky.com>
2023-06-02 12:30:29 +0800

    Implement CustomNSError in LatteError

--- Sources/LatteError.swift
+++ Sources/LatteError.swift
@@ -1,8 +1,25 @@
 import Foundation
 
 // Implement LocalizedError to customize localizedDescription
-enum LatteError: LocalizedError {
+enum LatteError: LocalizedError, CustomNSError {
     case unexpected(message: String)
+
+    public static var errorDomain: String { "LatteError" }
+    public var errorCode: Int {
+        switch self {
+        case .unexpected:
+            return 0
+        }
+    }
+
+    public var errorUserInfo: [String: Any] {
+        var info: [String: Any] = [:]
+        switch self {
+        case let .unexpected(msg):
+            info["message"] = msg
+        }
+        return info
+    }
 }
 
 extension LatteError {
