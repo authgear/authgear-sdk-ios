55a698696bcfb450ef0f20094f955887d43cdc4e
Louis Chan <louischan@oursky.com>
2023-11-01 14:47:32 +0800

    Apply 2023C01 changes on main

---
+++ Sources/Latte+EmailClient.swift
@@ -0,0 +1,57 @@
+import Foundation
+import UIKit
+
+public extension Latte {
+    struct EmailClient {
+        let name: String
+        let openURL: String
+
+        public static let mail = EmailClient(
+            name: "Mail",
+            openURL: "message://"
+        )
+
+        public static let gmail = EmailClient(
+            name: "Gmail",
+            openURL: "googlegmail://"
+        )
+    }
+
+    static func makeChooseEmailClientAlertController(
+        title: String,
+        message: String,
+        cancelLabel: String,
+        items: [EmailClient]
+    ) -> UIAlertController {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .actionSheet
+        )
+        let openableItems = items.filter { item in
+            guard let url = URL(string: item.openURL) else {
+                return false
+            }
+            return UIApplication.shared.canOpenURL(url)
+        }
+        for item in openableItems {
+            alert.addAction(UIAlertAction(
+                title: item.name,
+                style: .default,
+                handler: { _ in
+                    guard let url = URL(string: item.openURL) else {
+                        return
+                    }
+                    UIApplication.shared.open(url)
+                }
+            ))
+        }
+        alert.addAction(UIAlertAction(
+            title: cancelLabel,
+            style: .cancel,
+            handler: { _ in
+            }
+        ))
+        return alert
+    }
+}
---
+++ Sources/LatteError.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public enum LatteError: LocalizedError, CustomNSError {
+    case unexpected(message: String)
+    case invalidShortLink
+    case timeout
+
+    // Implements CustomNSError
+    public static var errorDomain: String { "LatteError" }
+    public var errorCode: Int {
+        switch self {
+        case .unexpected:
+            return 0
+        case .invalidShortLink:
+            return 1
+        case .timeout:
+            return 2
+        }
+    }
+
+    public var errorUserInfo: [String: Any] {
+        var info: [String: Any] = [:]
+        switch self {
+        case let .unexpected(msg):
+            info["message"] = msg
+        case .invalidShortLink:
+            info["message"] = "invalid short link"
+        case .timeout:
+            info["message"] = "timeout"
+        }
+        return info
+    }
+
+    // Implements LocalizedError
+    public var errorDescription: String? {
+        switch self {
+        case let .unexpected(message):
+            return message
+        case .invalidShortLink:
+            return "invalid short link"
+        case .timeout:
+            return "timeout"
+        }
+    }
+}
