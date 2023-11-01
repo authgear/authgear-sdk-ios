cffa7b8dc4a09bc9190155fa51f4f12eada4e719
Louis Chan <louischan@oursky.com>
2023-11-01 14:47:32 +0800

    Apply 2023C01 changes on main

---
+++ Sources/Latte+UniversalLink.swift
@@ -0,0 +1,167 @@
+import Foundation
+import UIKit
+
+public protocol LatteLinkHandler {}
+
+public struct ResetLinkHandler: LatteLinkHandler {
+    public let url: URL
+}
+
+public struct LoginLinkHandler: LatteLinkHandler {
+    public let url: URL
+
+    public func handle(latte: Latte, completion: @escaping (Result<Void, Error>) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        authgearFetch(urlSession: latte.urlSession, request: request) { result in
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+            case .success:
+                completion(.success(()))
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+public class LatteShortLinkExpander: NSObject, URLSessionDelegate, URLSessionTaskDelegate {
+    let url: URL
+    lazy var session: URLSession = .init(configuration: .default, delegate: self, delegateQueue: nil)
+
+    let universalLinkOrigin: URL
+    let rewriteUniversalLinkOrigin: URL?
+
+    init(
+        url: URL,
+        universalLinkOrigin: URL,
+        rewriteUniversalLinkOrigin: URL?
+    ) {
+        self.url = url
+        self.universalLinkOrigin = universalLinkOrigin
+        self.rewriteUniversalLinkOrigin = rewriteUniversalLinkOrigin
+        super.init()
+    }
+
+    public func expand(_ handler: @escaping (Result<LatteLinkHandler?, Error>) -> Void) {
+        func mainQueueHandler(_ result: Result<LatteLinkHandler?, Error>) {
+            DispatchQueue.main.async {
+                handler(result)
+            }
+        }
+        let task = session.dataTask(with: url) { _, response, error in
+            if let error = error {
+                return mainQueueHandler(.failure(wrapError(error: error)))
+            }
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 302,
+                  let location = response.allHeaderFields["Location"] as? String,
+                  let locationURL = URL(string: location)
+            else {
+                return mainQueueHandler(.failure(LatteError.invalidShortLink))
+            }
+            mainQueueHandler(.success(Latte.getUniversalLinkHandler(
+                incomingURL: locationURL,
+                universalLinkOrigin: self.universalLinkOrigin,
+                rewriteUniversalLinkOrigin: self.rewriteUniversalLinkOrigin
+            )))
+        }
+        task.resume()
+    }
+
+    public func urlSession(_: URLSession, task _: URLSessionTask, willPerformHTTPRedirection _: HTTPURLResponse, newRequest _: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
+        // Stop redirection
+        completionHandler(nil)
+    }
+}
+
+@available(iOS 13.0, *)
+public extension Latte {
+    internal static func getUniversalLinkHandler(
+        incomingURL: URL,
+        universalLinkOrigin: URL,
+        rewriteUniversalLinkOrigin: URL? = nil
+    ) -> LatteLinkHandler? {
+        guard incomingURL.origin() == universalLinkOrigin.origin(),
+              let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
+            return nil
+        }
+
+        guard let path = components.path else {
+            return nil
+        }
+
+        switch path {
+        case _ where path.hasSuffix("/reset_link"):
+            return ResetLinkHandler(url: components.url!)
+        case _ where path.hasSuffix("/login_link"):
+            let url: URL
+            if let rewriteUniversalLinkOrigin = rewriteUniversalLinkOrigin {
+                url = incomingURL.rewriteOrigin(origin: rewriteUniversalLinkOrigin)
+            } else {
+                url = incomingURL
+            }
+            return LoginLinkHandler(url: url)
+        default:
+            return nil
+        }
+    }
+
+    static func getUniversalLinkHandler(
+        userActivity: NSUserActivity,
+        universalLinkOrigin: URL,
+        rewriteUniversalLinkOrigin: URL? = nil
+    ) -> LatteLinkHandler? {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+              let incomingURL = userActivity.webpageURL else {
+            return nil
+        }
+
+        return getUniversalLinkHandler(
+            incomingURL: incomingURL,
+            universalLinkOrigin: universalLinkOrigin,
+            rewriteUniversalLinkOrigin: rewriteUniversalLinkOrigin
+        )
+    }
+
+    static func getShortLinkExpander(
+        userActivity: NSUserActivity,
+        shortLinkOrigin: URL,
+        universalLinkOrigin: URL,
+        rewriteShortLinkOrigin: URL? = nil,
+        rewriteUniversalLinkOrigin: URL? = nil
+    ) -> LatteShortLinkExpander? {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
+            return nil
+        }
+        guard let incomingURL = userActivity.webpageURL else {
+            return nil
+        }
+        guard incomingURL.origin() == shortLinkOrigin.origin(),
+              incomingURL.path.starts(with: "/s") else {
+            return nil
+        }
+        let url: URL
+        if let rewriteShortLinkOrigin = rewriteShortLinkOrigin {
+            url = incomingURL.rewriteOrigin(origin: rewriteShortLinkOrigin)
+        } else {
+            url = incomingURL
+        }
+        return LatteShortLinkExpander(
+            url: url,
+            universalLinkOrigin: universalLinkOrigin,
+            rewriteUniversalLinkOrigin: rewriteUniversalLinkOrigin
+        )
+    }
+}
+
+private extension URL {
+    func rewriteOrigin(origin: URL) -> URL {
+        var selfComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        let thatComponents = URLComponents(url: origin, resolvingAgainstBaseURL: true)!
+        selfComponents.scheme = thatComponents.scheme
+        selfComponents.host = thatComponents.host
+        selfComponents.port = thatComponents.port
+        return selfComponents.url!
+    }
+}
---
+++ Sources/Latte+WKWebView.swift
@@ -0,0 +1,226 @@
+import Foundation
+import UIKit
+import WebKit
+
+enum LatteBuiltInEvents: String {
+    case openEmailClient
+    case openSMSClient
+    case tracking
+    case ready
+    case reauthWithBiometric
+    case resetPasswordCompleted
+}
+
+enum LatteBuiltInSignals: String {
+    case resetPasswordCompleted
+}
+
+let initScript = """
+    document.addEventListener('latte:event',
+        function (e) {
+            window.webkit.messageHandlers.latteEvent.postMessage(e.detail);
+        }
+    )
+"""
+
+@available(iOS 13.0, *)
+protocol LatteWebViewDelegate: AnyObject {
+    func latteWebView(onEvent _: LatteWKWebView, event: LatteWebViewEvent)
+}
+
+@available(iOS 13.0, *)
+class LatteWKWebView: WKWebView, WKNavigationDelegate {
+    let request: LatteWebViewRequest
+    var onReady: ((_ webview: LatteWKWebView) -> Void)?
+    var onReauthWithBiometric: ((_ webview: LatteWKWebView) -> Void)?
+    var onResetPasswordCompleted: ((_ webview: LatteWKWebView) -> Void)?
+    var completion: ((_ webview: LatteWKWebView, _ result: Result<LatteWebViewResult, Error>) -> Void)?
+    weak var viewController: UIViewController?
+    weak var delegate: LatteWebViewDelegate?
+
+    override var inputAccessoryView: UIView? {
+        // Hide the accessory view
+        nil
+    }
+
+    private var initialNavigation: WKNavigation?
+
+    init(request: LatteWebViewRequest, isInspectable: Bool) {
+        self.request = request
+
+        super.init(frame: .zero, configuration: WKWebViewConfiguration())
+        if isInspectable {
+            // iOS 16.4 requires Xcode 14.3 to build.
+            // Xcode 14.3 requires macOS 13 to install.
+            // Compiling against iOS 16.4 requires a lot toolchain updates as of 2023-04-24.
+            // if #available(iOS 16.4, *) {
+            //     self.isInspectable = true
+            // } else {
+            //     self.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
+            // }
+            self.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
+        }
+
+        self.allowsBackForwardNavigationGestures = true
+        self.scrollView.alwaysBounceVertical = false
+        self.scrollView.alwaysBounceHorizontal = false
+        self.scrollView.contentInsetAdjustmentBehavior = .never
+
+        self.navigationDelegate = self
+
+        let userScript = WKUserScript(source: initScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+        self.configuration.userContentController.addUserScript(userScript)
+        self.configuration.userContentController.add(MessageHandler(self), name: "latteEvent")
+
+        _ = LatteWKWebView.oneTimeOnlySwizzle
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func load() {
+        self.initialNavigation = self.load(URLRequest(url: self.request.url))
+    }
+
+    func dispatchSignal(signal: LatteBuiltInSignals) {
+        self.evaluateJavaScript("""
+            document.dispatchEvent(
+              new CustomEvent("latte:signal", {
+                detail: { type: "\(signal.rawValue)" },
+              })
+            );
+        """)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        if navigation == self.initialNavigation {
+            self.completion?(self, .failure(error))
+            self.completion = nil
+        }
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        if navigation == self.initialNavigation {
+            self.completion?(self, .failure(error))
+            self.completion = nil
+        }
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+        if let navigationURL = navigationAction.request.url {
+            var parts = URLComponents(url: navigationURL, resolvingAgainstBaseURL: false)!
+            parts.query = nil
+            parts.fragment = nil
+            if parts.string == self.request.redirectURI {
+                let result = Result<LatteWebViewResult, Error>.success(
+                    LatteWebViewResult(finishURL: navigationURL)
+                )
+
+                self.completion?(self, result)
+                self.completion = nil
+                return .cancel
+            }
+        }
+
+        if #available(iOS 14.5, *) {
+            if navigationAction.shouldPerformDownload {
+                return .download
+            } else {
+                return .allow
+            }
+        } else {
+            return .allow
+        }
+    }
+
+    // Avoid reference cycle; ref https://stackoverflow.com/a/26383032
+    class MessageHandler: NSObject, WKScriptMessageHandler {
+        private weak var parent: LatteWKWebView?
+        init(_ parent: LatteWKWebView) {
+            self.parent = parent
+        }
+
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            guard let parent = self.parent else { return }
+
+            switch message.name {
+            case "latteEvent":
+                guard let body = message.body as? [String: Any] else { return }
+                guard let type = body["type"] as? String else { return }
+
+                switch type {
+                case LatteBuiltInEvents.openEmailClient.rawValue:
+                    parent.delegate?.latteWebView(onEvent: parent, event: .openEmailClient)
+                case LatteBuiltInEvents.openSMSClient.rawValue:
+                    parent.delegate?.latteWebView(onEvent: parent, event: .openSMSClient)
+                case LatteBuiltInEvents.tracking.rawValue:
+                    guard let event_name = body["event_name"] as? String else { return }
+                    guard let params = body["params"] as? [String: Any] else { return }
+                    let event = LatteTrackingEvent(
+                        event_name: event_name, params: params
+                    )
+                    parent.delegate?.latteWebView(onEvent: parent, event: .trackingEvent(event: event))
+                case LatteBuiltInEvents.ready.rawValue:
+                    parent.onReady?(parent)
+                    parent.onReady = nil
+                case LatteBuiltInEvents.reauthWithBiometric.rawValue:
+                    parent.onReauthWithBiometric?(parent)
+                case LatteBuiltInEvents.resetPasswordCompleted.rawValue:
+                    parent.onResetPasswordCompleted?(parent)
+                default:
+                    return
+                }
+            default:
+                break
+            }
+        }
+    }
+}
+
+// ref: https://github.com/ionic-team/capacitor/blob/89cddcd6497034146e0938ce8c264e22e7baba52/ios/Capacitor/Capacitor/WKWebView%2BCapacitor.swift#L22
+@available(iOS 13.0, *)
+extension LatteWKWebView {
+    typealias FiveArgClosureType = @convention(c) (Any, Selector, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void
+
+    static let oneTimeOnlySwizzle: () = swizzleKeyboardMethods()
+    private static func swizzleKeyboardMethods() {
+        let frameworkName = "WK"
+        let className = "ContentView"
+        guard let targetClass = NSClassFromString(frameworkName + className) else {
+            return
+        }
+
+        func findWebView(_ object: Any?) -> WKWebView? {
+            var view = object as? UIView
+            while view != nil {
+                if let webview = view as? WKWebView {
+                    return webview
+                }
+                view = view?.superview
+            }
+            return nil
+        }
+
+        func swizzleFiveArgClosure(_ method: Method, _ selector: Selector) {
+            let originalImp: IMP = method_getImplementation(method)
+            let original: FiveArgClosureType = unsafeBitCast(originalImp, to: FiveArgClosureType.self)
+            let block: @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
+                if let webView = findWebView(me), webView is LatteWKWebView {
+                    original(me, selector, arg0, true, arg2, arg3, arg4)
+                } else {
+                    original(me, selector, arg0, arg1, arg2, arg3, arg4)
+                }
+            }
+            let imp: IMP = imp_implementationWithBlock(block)
+            method_setImplementation(method, imp)
+        }
+
+        let selectorMkIV: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
+
+        if let method = class_getInstanceMethod(targetClass, selectorMkIV) {
+            swizzleFiveArgClosure(method, selectorMkIV)
+        }
+    }
+}
