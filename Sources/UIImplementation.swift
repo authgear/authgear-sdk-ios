9f73cd04115fbe79fdfabd8ace8754d8f26734b7
Louis Chan <louischan@oursky.com>
2024-02-19 15:34:55 +0800

    Put ASWebAuthenticationSessionUIImplementation in its own file

--- Sources/UIImplementation.swift
+++ Sources/UIImplementation.swift
@@ -1,28 +1,6 @@
-import AuthenticationServices
+import Foundation
 
 public protocol UIImplementation {
     typealias CompletionHandler = (Result<URL, Error>) -> Void
     func openAuthorizationURL(url: URL, redirectURI: URL, shareCookiesWithDeviceBrowser: Bool, completion: @escaping CompletionHandler)
-}
-
-public class ASWebAuthenticationSessionUIImplementation: NSObject, UIImplementation, ASWebAuthenticationPresentationContextProviding {
-    public func openAuthorizationURL(url: URL, redirectURI: URL, shareCookiesWithDeviceBrowser: Bool, completion: @escaping CompletionHandler) {
-        let session = ASWebAuthenticationSession(url: url, callbackURLScheme: redirectURI.scheme!) { url, error in
-            if let error = error {
-                completion(.failure(wrapError(error: error)))
-            }
-            if let url = url {
-                completion(.success(url))
-            }
-        }
-        if #available(iOS 13.0, *) {
-            session.prefersEphemeralWebBrowserSession = !shareCookiesWithDeviceBrowser
-            session.presentationContextProvider = self
-        }
-        session.start()
-    }
-
-    public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
-        UIApplication.shared.windows.filter { $0.isKeyWindow }.first!
-    }
 }
