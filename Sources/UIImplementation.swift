8d4a1e49a26f6041ec0ca325218de97e1c6d490e
Louis Chan <louischan@oursky.com>
2024-02-19 15:04:08 +0800

    Introduce UIImplementation

--- Sources/UIImplementation.swift
+++ Sources/UIImplementation.swift
@@ -1,54 +1,27 @@
 import AuthenticationServices
 
-protocol AuthenticationSession {
+public protocol UIImplementation {
     typealias CompletionHandler = (Result<URL, Error>) -> Void
-    @discardableResult func start() -> Bool
-    func cancel()
+    func openAuthorizationURL(url: URL, redirectURI: URL, shareCookiesWithDeviceBrowser: Bool, completion: @escaping CompletionHandler)
 }
 
-extension ASWebAuthenticationSession: AuthenticationSession {}
-
-class AuthenticationSessionProvider: NSObject {
-    func makeAuthenticationSession(
-        url: URL,
-        redirectURI: String,
-        prefersEphemeralWebBrowserSession: Bool?,
-        completionHandler: @escaping AuthenticationSession.CompletionHandler
-    ) -> AuthenticationSession {
-        let realCompletionHandler: (URL?, Error?) -> Void = { (url: URL?, error: Error?) in
+public class ASWebAuthenticationSessionUIImplementation: NSObject, UIImplementation, ASWebAuthenticationPresentationContextProviding {
+    public func openAuthorizationURL(url: URL, redirectURI: URL, shareCookiesWithDeviceBrowser: Bool, completion: @escaping CompletionHandler) {
+        let session = ASWebAuthenticationSession(url: url, callbackURLScheme: redirectURI.scheme!) { url, error in
             if let error = error {
-                return completionHandler(.failure(wrapError(error: error)))
+                completion(.failure(wrapError(error: error)))
             }
-
             if let url = url {
-                return completionHandler(.success(url))
+                completion(.success(url))
             }
         }
-
-        let session = ASWebAuthenticationSession(
-            url: url,
-            callbackURLScheme: getURIScheme(uri: redirectURI),
-            completionHandler: realCompletionHandler
-        )
-
         if #available(iOS 13.0, *) {
-            session.prefersEphemeralWebBrowserSession = prefersEphemeralWebBrowserSession ?? false
+            session.prefersEphemeralWebBrowserSession = !shareCookiesWithDeviceBrowser
             session.presentationContextProvider = self
         }
-
-        return session
+        session.start()
     }
 
-    private func getURIScheme(uri: String) -> String {
-        if let index = uri.firstIndex(of: ":") {
-            return String(uri[..<index])
-        }
-        return uri
-    }
-}
-
-extension AuthenticationSessionProvider: ASWebAuthenticationPresentationContextProviding {
-    @available(iOS 13.0, *)
     public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
         UIApplication.shared.windows.filter { $0.isKeyWindow }.first!
     }
