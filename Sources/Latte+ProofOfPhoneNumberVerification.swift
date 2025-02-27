8ebd8f1fda4b74b1584bd7124a4004ca2399bd04
Newman Chow <newman@oursky.com>
2025-02-27 18:41:00 +0800

    Refresh access token before fetching proof of phone number verification

--- Sources/Latte+ProofOfPhoneNumberVerification.swift
+++ Sources/Latte+ProofOfPhoneNumberVerification.swift
@@ -8,28 +8,34 @@ public extension Latte {
     func getProofOfPhoneNumberVerification(
         handler: @escaping (Result<String, Error>) -> Void
     ) {
-        var proofOfNumberVerificationEndpoint = self.middlewareEndpoint + "/proof_of_phone_number_verification"
-        var request = URLRequest(url: URL(string: proofOfNumberVerificationEndpoint)!)
-        request.httpMethod = "POST"
-
-        let jsonEncoder = JSONEncoder()
-        let body = try! jsonEncoder.encode([
-            "access_token": authgear.accessToken
-        ])
-        request.httpBody = body
-
-        authgearFetch(urlSession: urlSession, request: request) { result in
-            switch result {
-            case let .failure(error):
+        authgear.refreshAccessTokenIfNeeded {(result) in
+            if case let .failure(error) = result {
                 handler(.failure(error))
-            case let .success((respData, _)):
-                do {
-                    let decorder = JSONDecoder()
-                    decorder.keyDecodingStrategy = .convertFromSnakeCase
-                    let response = try decorder.decode(ProofOfPhoneNumberVerificationResponse.self, from: respData!)
-                    handler(.success(response.proofOfPhoneNumberVerification))
-                } catch {
+            }
+
+            var proofOfNumberVerificationEndpoint = self.middlewareEndpoint + "/proof_of_phone_number_verification"
+            var request = URLRequest(url: URL(string: proofOfNumberVerificationEndpoint)!)
+            request.httpMethod = "POST"
+
+            let jsonEncoder = JSONEncoder()
+            let body = try! jsonEncoder.encode([
+                "access_token": self.authgear.accessToken
+            ])
+            request.httpBody = body
+
+            authgearFetch(urlSession: self.urlSession, request: request) { result in
+                switch result {
+                case let .failure(error):
                     handler(.failure(error))
+                case let .success((respData, _)):
+                    do {
+                        let decorder = JSONDecoder()
+                        decorder.keyDecodingStrategy = .convertFromSnakeCase
+                        let response = try decorder.decode(ProofOfPhoneNumberVerificationResponse.self, from: respData!)
+                        handler(.success(response.proofOfPhoneNumberVerification))
+                    } catch {
+                        handler(.failure(error))
+                    }
                 }
             }
         }
